bb1c3d3a2350137d66323bb8812ca6934abffeef
d.sato <neuralpocket@google.com>
2021-01-13 09:11:26 +0900

    first commit.

---
+++ src/button.rs
@@ -0,0 +1,51 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+use yew::Properties;
+use yew::web_sys;
+
+#[derive(Clone, Debug, PartialEq, Properties)]
+pub struct Props {
+    pub label: &'static str,
+    pub value: f64,
+    // pub onchange: Callback<f64>,
+}
+
+pub struct Button {
+    props: Props,
+    link: ComponentLink<Self>,
+}
+impl Component for Button {
+    type Message = ();
+    type Properties = Props;
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            props,
+            link,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        let Props {
+            label,
+            value
+        } = self.props;
+
+        html! {
+            <>
+                <label>{ label }</label>
+                <button>{ value }</button>
+            </>
+        }
+    }
+}
